cf3e599e86da1526a9ccb48d43ee69e2fdfcd168
Abhishek Jadon <abhishekjadon173@gmail.com>
2026-04-14 22:29:19 +0530

    fixed the cors

--- server.js
+++ server.js
@@ -24,7 +24,10 @@ const io = new Server(server, {
 
 // MIDDLEWARE
 
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
